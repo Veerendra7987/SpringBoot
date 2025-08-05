330abffe9a0dfbb510c69cfe09dc610845af6c74
Veerendra7987 <ashuahirwar512@gmail.com>
2025-08-05 17:52:07 +0530

    Add files via upload

---
+++ src/Test/Test/src/main/java/com/example/Test/firstController.java
@@ -0,0 +1,33 @@
+package com.example.Test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@Controller
+public class firstController {
+    @GetMapping("/first")
+    public String first(Model m1){
+
+        m1.addAttribute("ashu","Veerendra");
+        m1.addAttribute("m2","Kuch nhi");
+        m1.addAttribute("m3",10);
+        m1.addAttribute("m4",20);
+        return "first";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model m2)
+    {
+        m2.addAttribute("profil","ashu");
+        return "profilepage";
+    }
+    @GetMapping("/profile1")
+    public String profile1(Model m3)
+    {
+        List<String> mylis= List.of("Java","Python","C++");
+        m3.addAttribute("Country",mylis);
+        return "profilepage";
+    }
+}
